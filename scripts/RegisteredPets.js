8cce11bce5381979103bd4f844b0ae743ecbe0fd
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-06-08 13:18:09 -0500

    updated RegisteredPets module by adding click event listener to display an alert that says which walker is walking which pet

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,16 +1,36 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js";
 
-const pets = getPets()
 
 export const RegisteredPets = () => {
+    const pets = getPets();
+    const walkers = getWalkers();
+
+    document.addEventListener(
+        "click", 
+        (event) => {
+            if (event.target.id.startsWith("pet")) {
+                const [, petId] = event.target.id.split("--")
+
+                for (const pet of pets) {
+                    if (pet.id === parseInt(petId)) {
+                        const foundWalker = walkers.find(walker => walker.id === pet.walkerId)
+
+                        window.alert(`${pet.name} is being walked by ${foundWalker.name}.`)    
+                    };
+                };
+            };
+        }            
+    );
+
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
-    }
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
+    };
 
-    petHTML += "</ul>"
+    petHTML += "</ul>";
 
-    return petHTML
-}
+    return petHTML;
+
+};
 
